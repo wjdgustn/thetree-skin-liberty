e0f6ce029eecd0fa198747a2474f6ea5fe94c8ca
navyCarpet <125118599+navyCarpet@users.noreply.github.com>
2024-06-05 17:32:14 +0900

    Add user document btn

--- components/contentTool.vue
+++ components/contentTool.vue
@@ -24,10 +24,11 @@
             <nuxt-link v-if="toolList.includes('acl')" :to="doc_action_link($store.state.page.data.document, 'acl')" class="btn btn-secondary tools-btn">ACL</nuxt-link>
             <nuxt-link v-if="toolList.includes('delete')" :to="doc_action_link($store.state.page.data.document, 'delete')" class="btn btn-danger tools-btn">삭제</nuxt-link>
             <nuxt-link v-if="toolList.includes('move')" :to="doc_action_link($store.state.page.data.document, 'move')"  class="btn btn-secondary tools-btn">이동</nuxt-link>
+            <nuxt-link v-if="toolList.includes('userdoc')" :to="doc_action_link($store.state.page.data.account.name, 'w')" class="btn btn-secondary tools-btn">사용자 문서</nuxt-link>
             <template v-if="toolList.includes('contribution') || toolList.includes('raw') || toolList.includes('blame') || toolList.includes('diff') || toolList.includes('revert') || toolList.includes('menu')">
                 <button type="button" class="btn btn-secondary tools-btn dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="caret"></span></button>
                 <div class="dropdown-menu dropdown-menu-right" role="menu">
-                    <nuxt-link v-if="toolList.includes('contribution')" :to="contribution_link($store.state.page.data.user.uuid)" class="dropdown-item">기여 내역</nuxt-link>
+                    <nuxt-link v-if="toolList.includes('contribution')" :to="contribution_link($store.state.page.data.user.uuid)" class="dropdown-item">기여 목록</nuxt-link>
                     <nuxt-link v-if="toolList.includes('raw')" :to="doc_action_link($store.state.page.data.document, 'raw', rev ? { rev } : undefined)" class="dropdown-item">RAW</nuxt-link>
                     <nuxt-link v-if="toolList.includes('blame')" :to="doc_action_link($store.state.page.data.document, 'blame', rev ? { rev } : undefined)" class="dropdown-item">Blame</nuxt-link>
                     <nuxt-link v-if="toolList.includes('diff')" :to="doc_action_link($store.state.page.data.document, 'diff', rev ? { rev, oldrev: rev - 1 } : undefined)" class="dropdown-item">이전 리버전과 비교</nuxt-link>
@@ -88,6 +89,10 @@ export default {
                 case 'edit_request':
                 case 'edit_request_close':
                     tools.push('discuss');
+                    break;
+                case 'contribution':
+                case 'contribution_discuss':
+                    if (this.$store.state.page.data.account.type === 1) tools.push('userdoc');
                     break;
             }
             if (this.$store.state.page.data.menus?.length) tools.push('menu');
