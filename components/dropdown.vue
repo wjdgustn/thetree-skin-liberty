6c0f106963b184e33d5e211a8159cc6b70431aa8
navyCarpet <125118599+navyCarpet@users.noreply.github.com>
2024-08-09 21:40:03 +0900

    Add dropdown backdrop

--- components/dropdown.vue
+++ components/dropdown.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="dropdown" :class="{ 'open': show }">
-        <div @click="toggle">
-            <slot name="toggle"></slot>
-        </div>
+    <div ref="dropdown" class="dropdown" :class="{ 'open': show }">
+        <div @click="toggle"><slot name="toggle"></slot></div>
         <slot v-if="show"></slot>
+        <div v-if="show" class="backdrop" @click="hide"></div>
     </div>
 </template>
+
+<style scoped>
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 999;
+}
+</style>
 
 <script>
 export default {
@@ -17,6 +27,9 @@ export default {
     methods: {
         toggle() {
             this.show = !this.show;
+        },
+        hide() {
+            this.show = false;
         }
     }
 }
