b28fc5a4c652ae9c5df2ca737de9ab04d7c90b57
navyCarpet <125118599+navyCarpet@users.noreply.github.com>
2024-11-03 17:43:16 +0900

    Fix syntax

--- layouts/contentTool.vue
+++ layouts/contentTool.vue
@@ -178,7 +178,7 @@ export default {
     },
     methods: {
         calculate() {
-            const uuid = this.data.uuid;
+            const uuid = this.data?.uuid;
             this.main = [];
             this.menu = [];
             switch (this.$store.state.page.viewName) {
